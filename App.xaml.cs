814b074465d1f7c757a27b5f7127b10365a04c35
Ahmed Shamel <ahmedshamelaltaie@gmail.com>
2024-12-30 12:10:41 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+using EClinicMaui.SQLServer.Pages;
+
+namespace EClinicMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Sidebar()
+            {
+                FlowDirection = FlowDirection.LeftToRight
+            };
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
